6d35554eddd3e29db23525f562e6aa6c7c632e3e
Zi Han Ding <zid8@pitt.edu>
2023-09-16 11:10:23 -0400

    added mongoDB integration

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    MONGO_URL = os.environ.get('MONGO_URL')
+    MONGO_URI = os.environ.get('MONGO_URI')
