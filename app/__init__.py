75482e59c2ae1a13167e49ac01f14f74a634ecc4
Zi Han Ding <zid8@pitt.edu>
2023-09-15 23:28:18 -0400

    added temporary main page

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,12 @@ def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
+    #initialize the flask extensions here
+
+    #register the blueprints here
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp)
+
     @app.route('/test')
     def index():
         return "Hello World!"
